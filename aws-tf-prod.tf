fdfd5135c1554a5d18864a40041bbd62ac06b181
AnatoliiTymoshchuk <42546387+AnatoliiTymoshchuk@users.noreply.github.com>
2020-11-07 13:31:01 +0200

    Update aws-tf-prod.tf

--- aws-tf-prod.tf
+++ aws-tf-prod.tf
@@ -4,7 +4,7 @@ provider "aws" {
   
 resource "aws_instance" "test_VM" {
   # CentOS Linux 6 x86_64 HVM EBS 1703_011
-  ami           = "ami-0ea7202ad5ad9bdb7" 
+  ami           = "ami-09e33dae3dfb2947a" 
   instance_type = "t2.small"
   availability_zone = "eu-west-2a"
 
